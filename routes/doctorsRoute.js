32fb2ac8c7124274532ce40689294cb3e637bb98
johno06 <85436076+johno06@users.noreply.github.com>
2023-06-02 11:19:49 -0600

    Generating RTC-Token(MongoDB) done

--- routes/doctorsRoute.js
+++ routes/doctorsRoute.js
@@ -5,6 +5,27 @@ const authMiddleware = require("../middleware/authMiddleware");
 const Appointment = require("../models/appointmentModel");
 const User = require("../models/userModels");
 
+
+
+
+router.post ('/get-user-info-by-id', async (req, res) => {
+  try {
+    // const user = await User.findOne({ userId: req.body.userId });
+    const user = await User.findById ({_id: req.body._id});
+    res.status (200).send ({
+      success: true,
+      message: 'Doctor info fetched successfully',
+      data: user,
+    });
+  } catch (error) {
+    res
+      .status (500)
+      .send ({message: 'Error getting doctor info', success: false, error});
+  }
+});
+
+
+
 router.post("/get-doctor-info-by-user-id", async (req, res) => {
   try {
     const doctor = await Doctor.findOne({ userId: req.body.userId });
@@ -128,6 +149,20 @@ router.post("/change-appointment-status", authMiddleware, async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Error changing appointment status", success: false, error });
+  }
+});
+
+router.patch ('/updateRtcToken/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updates = req.body;
+    const options = {new: true};
+
+    const result = await User.findByIdAndUpdate (id, updates, options);
+    res.send (result);
+  } catch (error) {
+    console.log (error.message);
+    // res.json({message:'email is already used'})
   }
 });
 
