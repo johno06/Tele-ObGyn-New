083f831a2d9745084787a8eaaeb7b9189fad6d56
johno06 <85436076+johno06@users.noreply.github.com>
2023-06-13 12:30:45 -0600

    debug

--- server.js
+++ server.js
@@ -51,8 +51,6 @@ app.get ('*', (req, res) => {
   res.sendFile (path.join (__dirname, 'client', 'public', 'index.html'));
 });
 
-
-
 const port = process.env.PORT || 5000;
 
 app.listen (port, () => console.log (`Listening on port ${port}`));
