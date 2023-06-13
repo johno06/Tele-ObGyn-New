ce74197e4e8eaf280bf9dceae35a249af837ccbf
johno06 <85436076+johno06@users.noreply.github.com>
2023-06-13 10:20:53 -0600

    add cors in server

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
-require("dotenv").config();
-const dbConfig = require("./config/dbConfig");
-const cors = require("cors")
-const express = require("express");
+require ('dotenv').config ();
+const dbConfig = require ('./config/dbConfig');
+const cors = require ('cors');
+const express = require ('express');
 
+const app = express ();
+app.use (express.json ());
 
+app.use (
+  cors ({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'https://fuentes-clinic-website.onrender.com',
+    ],
+  })
+);
 
-const app = express();
-app.use(express.json());
-app.use(cors({
-  credentials:true,
-  origin:["http://localhost:3000", "https://fuentes-clinic.onrender.com"]
-}));
+// Set Access-Control-Allow-Origin header
+app.use ((req, res, next) => {
+  res.setHeader (
+    'Access-Control-Allow-Origin',
+    'https://fuentes-clinic-website.onrender.com'
+  );
+  // Add other CORS headers if needed
+  // ...
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  next ();
 });
 
-const userRoute = require("./routes/userRoute");
-const adminRoute = require("./routes/adminRoute");
-const doctorRoute = require("./routes/doctorsRoute");
-const utilityRoute = require("./routes/utilityRoute");
+app.get ('/', (req, res) => {
+  res.send ('Hello World!');
+});
 
-app.use("/api/user", userRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/doctor", doctorRoute);
-app.use("/api/utility", utilityRoute);
+const userRoute = require ('./routes/userRoute');
+const adminRoute = require ('./routes/adminRoute');
+const doctorRoute = require ('./routes/doctorsRoute');
+const utilityRoute = require ('./routes/utilityRoute');
+
+app.use ('/api/user', userRoute);
+app.use ('/api/admin', adminRoute);
+app.use ('/api/doctor', doctorRoute);
+app.use ('/api/utility', utilityRoute);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen (port, () => console.log (`Listening on port ${port}`));
