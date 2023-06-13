b3db63aa9f8d8ac175b8541b30263819ad2603f8
johno06 <85436076+johno06@users.noreply.github.com>
2023-06-13 17:20:33 -0600

    redo again

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use ('/api/utility', utilityRoute);
 app.use (express.static (path.join (__dirname, 'client', 'build')));
 
 // Fallback route for client-side routing
-app.get ('/*', (req, res) => {
+app.get ('*', (req, res) => {
   res.sendFile (path.join (__dirname, 'client', 'build', 'index.html'));
 });
 
